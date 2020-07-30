2ac27822007f7952ac0c97cf517a5122bc405170
Peter Williams <peter@newton.cx>
2020-07-30 13:49:26 -0400

    wwt_data_formats/tests/test_server.py: add a real test

--- wwt_data_formats/tests/test_server.py
+++ wwt_data_formats/tests/test_server.py
@@ -4,11 +4,33 @@
 
 from __future__ import absolute_import, division, print_function
 
-from http.server import HTTPServer as HTTPServerClass
+from argparse import Namespace
+from http.server import HTTPServer
 import pytest
+import requests
+from threading import Thread
 
 from . import test_path
-from .. import server
+from .. import folder, server
+
+
+class TestServer(object):
+    @classmethod
+    def setup_class(cls):
+        cls.server_port = 33007 # just hope it's not taken
+
+        settings = Namespace()
+        settings.port = cls.server_port
+        settings.root_dir = test_path()
+
+        cls.thread = Thread(target=lambda: server.run_server(settings))
+        cls.thread.setDaemon(True)
+        cls.thread.start()
+
+    def test_wtml_rewrite(self):
+        base_url = 'http://localhost:{}/'.format(self.server_port)
+        f = folder.Folder.from_url(base_url + 'test1.wtml')
+        assert f.children[0].thumbnail == base_url + 'thumb.jpg'
 
 
 def test_smoke():
@@ -16,5 +38,5 @@ def test_smoke():
 
     server_address = ('', 0)
 
-    with HTTPServerClass(server_address, server.WWTRequestHandler) as httpd:
+    with HTTPServer(server_address, server.WWTRequestHandler) as httpd:
         pass
