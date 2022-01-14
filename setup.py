fa1505dd68742f7eb1a361b5d3a0f312cca7f61d
Peter Williams <peter@newton.cx>
2022-01-14 10:21:05 -0500

    setup.py: reformat with "black"

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # -*- mode: python; coding: utf-8 -*-
-# Copyright 2019-2020 the .NET Foundation
+# Copyright 2019-2022 the .NET Foundation
 # Licensed under the MIT License
 
 from __future__ import absolute_import, division, print_function
@@ -7,86 +7,83 @@ from __future__ import absolute_import, division, print_function
 import os
 from setuptools import setup, Extension
 
+
 def get_long_desc():
     in_preamble = True
     lines = []
 
-    with open('README.md', 'rt', encoding='utf8') as f:
+    with open("README.md", "rt", encoding="utf8") as f:
         for line in f:
             if in_preamble:
-                if line.startswith('<!--pypi-begin-->'):
+                if line.startswith("<!--pypi-begin-->"):
                     in_preamble = False
             else:
-                if line.startswith('<!--pypi-end-->'):
+                if line.startswith("<!--pypi-end-->"):
                     break
                 else:
                     lines.append(line)
 
-    lines.append('''
+    lines.append(
+        """
 
 For more information, including installation instructions, please visit [the
 project homepage].
 
 [the project homepage]: https://wwt-data-formats.readthedocs.io/
-''')
-    return ''.join(lines)
+"""
+    )
+    return "".join(lines)
 
 
 setup_args = dict(
-    name = 'wwt_data_formats',  # cranko project-name
-    version = '0.dev0',  # cranko project-version
-    description = 'Low-level interface to AAS WorldWide Telescope data formats',
-    long_description = get_long_desc(),
-    long_description_content_type = 'text/markdown',
-    url = 'https://wwt-data-formats.readthedocs.io/',
-    license = 'MIT',
-    platforms = 'Linux, Mac OS X',
-
-    author = 'AAS WorldWide Telescope Team',
-    author_email = 'wwt@aas.org',
-
-    classifiers = [
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Scientific/Engineering :: Astronomy',
-        'Topic :: Scientific/Engineering :: Visualization',
+    name="wwt_data_formats",  # cranko project-name
+    version="0.dev0",  # cranko project-version
+    description="Low-level interface to AAS WorldWide Telescope data formats",
+    long_description=get_long_desc(),
+    long_description_content_type="text/markdown",
+    url="https://wwt-data-formats.readthedocs.io/",
+    license="MIT",
+    platforms="Linux, Mac OS X",
+    author="AAS WorldWide Telescope Team",
+    author_email="wwt@aas.org",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "Topic :: Scientific/Engineering :: Visualization",
     ],
-
-    packages = [
-        'wwt_data_formats',
-        'wwt_data_formats.tests',
+    packages=[
+        "wwt_data_formats",
+        "wwt_data_formats.tests",
     ],
-    include_package_data = True,
-
-    entry_points = {
-        'console_scripts': [
-            'wwtdatatool=wwt_data_formats.cli:entrypoint',
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "wwtdatatool=wwt_data_formats.cli:entrypoint",
         ],
     },
-
-    install_requires = [
-        'requests',
-        'traitlets',
+    install_requires=[
+        "requests",
+        "traitlets",
     ],
-
-    extras_require = {
-        'test': [
-            'beautifulsoup4',
-            'mock',
-            'pytest-cov',
+    extras_require={
+        "test": [
+            "beautifulsoup4",
+            "mock",
+            "pytest-cov",
         ],
-        'docs': [
-            'astropy-sphinx-theme',
-            'numpydoc',
-            'sphinx',
-            'sphinx-automodapi',
+        "docs": [
+            "astropy-sphinx-theme",
+            "numpydoc",
+            "sphinx",
+            "sphinx-automodapi",
         ],
     },
 )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup(**setup_args)
